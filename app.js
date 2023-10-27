9f4ce641f6d48d6ed304c1ad8c292a0a9936a06d
Manish <mjpatel84@gmail.com>
2023-10-27 08:41:50 -0400

    adds downloadCode function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const textInput = document.getElementById("qr-code-input");
 const submitBtn = document.getElementById("submit-btn");
 const form = document.querySelector("form");
 const codeContainer = document.getElementById("code-container");
+const downloadBtn = document.getElementById("download-btn");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,4 +14,17 @@ form.addEventListener("submit", (event) => {
 function getQRCode(url) {
   codeContainer.innerHTML = "";
   const qrCode = new QRCode(codeContainer, url);
+  codeContainer.querySelector("img").setAttribute("id", "qr-code-image");
 }
+
+function downloadCode() {
+  const qrCodeImg = document.querySelector("#qr-code-image");
+  if (qrCodeImg) {
+    const link = document.createElement("a");
+    link.href = qrCodeImg.src;
+    link.download = "qrcode.png";
+    link.click();
+  }
+}
+
+downloadBtn.addEventListener("click", downloadCode);
